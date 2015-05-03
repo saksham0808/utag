4f1063f0ae2998073608752d689aec695157a13c
Saksham Sharma <saksham0808@gmail.com>
2015-05-04 03:43:41 +0530

    Removes file extension. Cleaned up "official" code.

--- utag.sh
+++ utag.sh
@@ -3,12 +3,25 @@
 for file in "$@"
 do
 	printf '******************************\n'
-	artist=$(echo $file | grep -E "^.*\ -\ " -o | grep -E "[^\-]+" -o )
+	printf 'File\t:\t%s\n' "$file"
+	artist=$(echo $file | grep -E "^.*\ -\ " -o | sed 's/ - $//g')
 	printf 'Artist\t:\t%s\n' "$artist"
+
+	# Retrieving text after hyphen
 	posthyphen=$(echo $file | grep -E "\-\ .+" -o | sed 's/^ *- *//g')
 	printf 'hyphen\t:\t%s\n' "$posthyphen"
-	#song=$(echo $posthyphen | sed 's/ft\..*//g' | sed 's/(official.*)//gI')
+	
+	# Removing all text after featuring
 	song=$(echo $posthyphen | sed 's/ft\..*//gI' | sed 's/feat\..*//gI')
+
+	# Removing the "official music video" text
 	song=$(echo $song 		| sed 's/(official.*)//gI')
+	song=$(echo $song 		| sed 's/\[official.*\]//gI')
+
+	# Removing file extension
+	song=$(echo $song		| sed 's/ *\.mp4//gI')
+	song=$(echo $song		| sed 's/ *\.mp3//gI')
+
+	
 	printf 'Song\t:\t%s\n' "$song"
 done
