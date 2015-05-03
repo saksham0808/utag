4339e87a495059666f5925c6fde29e663cfe297c
Saksham Sharma <saksham0808@gmail.com>
2015-05-04 03:28:14 +0530

    Handles all featuring now. Script made saner.

--- utag.sh
+++ utag.sh
@@ -2,10 +2,13 @@
 
 for file in "$@"
 do
+	printf '******************************\n'
 	artist=$(echo $file | grep -E "^.*\ -\ " -o | grep -E "[^\-]+" -o )
 	printf 'Artist\t:\t%s\n' "$artist"
-	posthyphen=$(echo $file | grep -E "\-\ .+" -o | grep -E "[^\-]+" -o | sed 's/^ *//g')
+	posthyphen=$(echo $file | grep -E "\-\ .+" -o | sed 's/^ *- *//g')
 	printf 'hyphen\t:\t%s\n' "$posthyphen"
-	song=$(echo $posthyphen | sed 's/ft\..*//g' | sed -e 's/(Official.*)'//g)
+	#song=$(echo $posthyphen | sed 's/ft\..*//g' | sed 's/(official.*)//gI')
+	song=$(echo $posthyphen | sed 's/ft\..*//gI' | sed 's/feat\..*//gI')
+	song=$(echo $song 		| sed 's/(official.*)//gI')
 	printf 'Song\t:\t%s\n' "$song"
 done
